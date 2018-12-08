a60bed60741ac82dab342a392b9dbf986863f51b
Dawid <dawidadamczykeit@gmail.com>
2018-12-08 08:34:57 +0100

    added primary template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
