44f95eba99c3a5ce8a9c02321023d0e5392737fe
Dawid <dawidadamczykeit@gmail.com>
2018-12-09 09:15:50 +0100

    a small change of town.index

--- resources/views/town/index.blade.php
+++ resources/views/town/index.blade.php
@@ -4,7 +4,7 @@
     <h2>These are the cities where our employees live</h2>
     <div class="list-group">
       @foreach($towns as $town)
-      <a href="/town/show/{{$town->id}}" class="list-group-item list-group-item-action">{{$town->name}}</a>
+      <a href="/town/{{$town->id}}" class="list-group-item list-group-item-action">{{$town->name}}</a>
       @endforeach
     </div>
 @endsection
